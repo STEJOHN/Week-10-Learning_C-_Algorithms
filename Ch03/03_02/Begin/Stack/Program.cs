68dc0728a29d224fb4623952639631c3f23a0645
Stephen Johnson <stephenjayyy@gmail.com>
2021-09-20 18:24:47 -0500

    Adds stack

--- Ch03/03_02/Begin/Stack/Program.cs
+++ Ch03/03_02/Begin/Stack/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Stack
 {
@@ -11,6 +12,39 @@ namespace Stack
         static void Main(string[] args)
         {
             //TODO
+        }
+    }
+
+    public class Stack
+    {
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new string[maxSize];
+            top = -1;
+        }
+
+        public void Push(string m)
+        {
+            if (isFull())
+            {
+
+                WriteLine("This stack is full");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = m;
+            }
+        }
+
+        private bool isFull()
+        {
+            return (maxSize - 1 == top);
         }
     }
 }
