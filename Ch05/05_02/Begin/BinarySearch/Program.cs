a81dbce72041222930bbcfa71f8824789b9de114
Stephen Johnson <stephenjayyy@gmail.com>
2021-09-20 23:08:03 -0500

    Adds binary search

--- Ch05/05_02/Begin/BinarySearch/Program.cs
+++ Ch05/05_02/Begin/BinarySearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace BinarySearch
 {
@@ -6,6 +7,45 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int thevalue = 43;
+            int[] array = new int[] { 11, 22, 43, 54, 57, 59, 62, 78 };
+
+            WriteLine("Our array contains: ");
+            Array.ForEach(array, x => Write(x + " "));
+
+            Write($"\n\nThe result of a binary search for {thevalue} is: ");
+            WriteLine(BinarySearch(array, thevalue));
+
+            ReadKey();
+
+        }
+
+        // a = array
+        //  x = what we are searching
+        //  p  = first index
+        //   q = midpoint
+        //    r = last index
+
+        public static int BinarySearch(int[] a, int x)
+        {
+            int p = 0;
+            int r = a.Length - 1;
+
+            while (p <= r)
+            {
+                int q = (p + r) / 2;
+                if (x < a[q])
+
+                    r = q - 1;
+
+                else if (x > a[q])
+
+                    p = q + 1;
+
+                else return q;
+            }
+
+            return -1;
         }
     }
 }
